abfa44ff27e68cb8b9dfd107fdb5a0ed4ef7f867
Andreea Manea <andreeasky@yahoo.com>
2018-06-21 09:55:24 +0300

    Espresso Testing

--- app/src/androidTest/java/com/example/user/bakingapp/SelectRecipeActivityScreenTest.java
+++ app/src/androidTest/java/com/example/user/bakingapp/SelectRecipeActivityScreenTest.java
@@ -1,18 +1,18 @@
 package com.example.user.bakingapp;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.anything;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static com.example.user.bakingapp.TestUtils.atPosition;
 
 @RunWith(AndroidJUnit4.class)
 
@@ -21,8 +21,8 @@ public class SelectRecipeActivityScreenTest {
     // Add the rule that provides functional testing of a single activity
     public static final String RECIPE_NAME = "Nutella Pie";
 
-// Finish writing this test which will click on a gridView Tea item and verify that
-    // the OrderActivity opens up with the correct name displayed.
+    // Finish writing this test which will click on a cardView recipe item and verify that
+    // the DetailsActivity opens up.
     /**
      * The ActivityTestRule is a rule provided by Android used for functional testing of a single
      * activity. The activity that will be tested will be launched before each test that's annotated
@@ -33,23 +33,17 @@ public class SelectRecipeActivityScreenTest {
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>( MainActivity.class );
 
     /**
-     * Clicks on a GridView item and checks it opens up the OrderActivity with the correct details.
-     *
+     * Clicks on a CardView item and checks if it opens up the DetailsActivity with the correct details.
      */
     @Test
-    public void clickGridViewItem_OpensDetailsActivity() {
+    public void clickRecyclerViewItem_OpensDetailsActivity() {
 
-        // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
-        // gridview item and clicks it.
-        onData( anything() ).inAdapterView( withId( R.id.card_view) ).atPosition( 1 ).perform( click() );
+        onView( withId( R.id.recycler_view ) )
+                .check( matches( atPosition( 0, hasDescendant( withText( RECIPE_NAME ) ) )) );
 
-        // Checks that the OrderActivity opens with the correct tea name displayed
-        onView( withId( R.id. recipe_name ) ).check( matches( withText( RECIPE_NAME ) ) );
-
+        // Checks that the DetailsActivity opens
+        onView( withId( R.id.recycler_view ) )
+                .perform( RecyclerViewActions.actionOnItemAtPosition( 0, click() ) );
     }
 
-    private class ActivityTestRule<T> {
-        public ActivityTestRule(Class<T> mainActivityClass) {
-        }
-    }
 }
---
+++ app/src/androidTest/java/com/example/user/bakingapp/TestUtils.java
@@ -0,0 +1,36 @@
+package com.example.user.bakingapp;
+
+import android.support.annotation.NonNull;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+import org.junit.runner.Description;
+
+import static android.support.test.espresso.core.internal.deps.dagger.internal.Preconditions.checkNotNull;
+
+public class TestUtils {
+
+    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
+        checkNotNull(itemMatcher);
+        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
+
+            @Override
+            public void describeTo(org.hamcrest.Description description) {
+                description.appendText("has item at position " + position + ": ");
+                itemMatcher.describeTo(description);
+            }
+
+            @Override
+            protected boolean matchesSafely(final RecyclerView view) {
+                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
+                if (viewHolder == null) {
+                    // has no item on such position
+                    return false;
+                }
+                return itemMatcher.matches(viewHolder.itemView);
+            }
+        };
+    }
+}
